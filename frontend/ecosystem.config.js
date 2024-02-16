429fac78a1e4babeb640c2e7f1223a3e75bf454e
M0riaz <118534241+M0riaz@users.noreply.github.com>
2024-02-16 18:39:22 +0300

    add pm2

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+// FRONT
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'frontend-service',
+    script: './dist/app.js',
+  }],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/M0riaz/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm install && npm run build',
+    },
+  },
+};
