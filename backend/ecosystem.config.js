d200d9f67f85b003b7e1ffdfbc67a500815cf0cc
M0riaz <118534241+M0riaz@users.noreply.github.com>
2024-02-17 14:32:23 +0300

    deploy back

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -7,23 +7,24 @@ const {
   DEPLOY_USER,
   DEPLOY_HOST,
   DEPLOY_PATH,
-  DEPLOY_REF = 'origin/master',
+  DEPLOY_REF,
+  DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
   apps: [{
-    name: 'backend-service',
-    script: './dist/app.js',
+    name: 'mesto',
+    script: 'dist/app.js',
   }],
   deploy: {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/M0riaz/web-plus-pm2-deploy.git',
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js',
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': ' cd backend && pwd npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
